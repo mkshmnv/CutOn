45159f4b9bc4fd323064329227927c1660289cca
Mykola <nickkosh@gmail.com>
2023-03-25 12:44:01 +0200

    done UI Authorization Screen

---
+++ app/src/main/java/com/example/cuton/LogoutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cuton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_logou)
+    }
+
+}
