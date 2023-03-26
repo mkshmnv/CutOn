f63bf1bc4ed95a95d4a3eb284ca3b3a5853259fa
Mykola <nickkosh@gmail.com>
2023-03-26 13:44:16 +0300

    refactor InitializeScreenActivity.kt

---
+++ app/src/main/java/com/example/cuton/InitializeScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.cuton
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cuton.databinding.ActivityInitializeScreenBinding
+
+@Suppress("DEPRECATION")
+class InitializeScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInitializeScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityInitializeScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.imageViewLogo.alpha = 0f
+        binding.imageViewLogo.animate().setDuration(1500).alpha(1f).withEndAction {
+            val intent = Intent(this, AuthorizationScreenActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
