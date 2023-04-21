39b5f9faa349c6e854dcea2433eb70616c1a3362
Mykola <nickkosh@gmail.com>
2023-04-21 13:41:44 +0300

    implement get response API JSON

--- app/src/main/java/com/example/cuton/InitializeScreenActivity.kt
+++ app/src/main/java/com/example/cuton/InitializeScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cuton
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
@@ -11,22 +12,35 @@ import android.os.Looper
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cuton.databinding.ActivityInitializeScreenBinding
+import okhttp3.*
+import okio.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 @Suppress("DEPRECATION")
 class InitializeScreenActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityInitializeScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initialize_screen)
+
+        binding = ActivityInitializeScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val appName = "cuton"
         val v = 36
+        val apiAddress: String
 
         if (!checkForInternet(this)) {
             Toast.makeText(this, "Не вдалось підключитись", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this, "Вдалось підключитись", Toast.LENGTH_SHORT).show()
         }
+
+        apiAddress = fetchCurrencyData(appName, v)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -38,12 +52,14 @@ class InitializeScreenActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, 1500) // This the delayed time in milliseconds.
+
     }
 
     private fun checkForInternet(context: Context): Boolean {
 
         // register activity with the connectivity manager service
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         // if the android version is equal to M
         // or greater we need to use the
@@ -77,5 +93,33 @@ class InitializeScreenActivity : AppCompatActivity() {
             @Suppress("DEPRECATION")
             return networkInfo.isConnected
         }
+    }
+
+    private fun fetchCurrencyData(name: String, ver: Int) : String {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://cr-test-ribu2uaqea-ey.a.run.app/routes/?appName=$name&v=$ver")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+//                    for ((name, value) in response.headers) {
+//                        println("$name: $value")
+//                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
+
+        return "route from API"
     }
 }
